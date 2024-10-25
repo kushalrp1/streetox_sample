a6f207935a18d33782420cd595ff1b50eaa093c5
kushalrp1 <krp37150@gmail.com>
2024-10-25 11:46:03 +0530

    Update Constants.kt

--- Streetox-master/app/src/main/java/com/streetox/streetox/fragments/oxbox/Constants.kt
+++ Streetox-master/app/src/main/java/com/streetox/streetox/fragments/oxbox/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAfTHGUac:APA91bG7NVYpLN3dgTKfcj1mCGl9u5__olaCMwDhQHG6iWeF7Bcw7zFv6dWPHLufVOjBOQ7Sz3MEhFWyLBgjZjufT294kono-tt0RKCBS5bXb1eJ0rvwRHgcYiG2tjAlcgNHTzrzMHEj"
+        const val SERVER_KEY = ""
         const val CONTENT_TYPE = "application/json"
     }
 }
